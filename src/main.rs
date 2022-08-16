12099721f66d5a635090e73bb3b1a63904492674
shockylemi <shockylemi@gmail.com>
2022-08-16 18:14:14 +0430

    bevy setup

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,48 @@
-fn main () {
+use bevy::prelude::*;
 
-  let i = "HELLO";
+struct GreetTimer(Timer);
 
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in query.iter() {
+            println!("hello {}!", name.0);
+        }
+    }
+}
 
-  let j = "WORLD";
+#[derive(Component)]
+struct Person;
 
+#[derive(Component)]
+struct Name(String);
 
-  let c = format!("{i} {j}");
-  
+fn add_people(mut commands: Commands) {
+    commands
+        .spawn()
+        .insert(Person)
+        .insert(Name("Elaina Proctor".to_string()));
+    commands
+        .spawn()
+        .insert(Person)
+        .insert(Name("Renzo Hume".to_string()));
+    commands
+        .spawn()
+        .insert(Person)
+        .insert(Name("Zayna Nieves".to_string()));
+}
 
-  println!("{c}");
-  
+pub struct HelloPlugin;
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_startup_system(add_people).add_system(greet_people);
+    }
+}
+
+fn main() {
+    App::new()
+        .insert_resource(GreetTimer(Timer::from_seconds(2.0, true)))
+        .add_plugins(DefaultPlugins)
+        .add_plugin(HelloPlugin)
+        .run();
 }
