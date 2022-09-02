3211c126671ee4b55fe207c6cd74b7de97d31115
CedricMoore <shockylemi@gmail.com>
2022-09-02 09:38:58 -0400

    vector stack

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,42 @@
+// Stacks
+
+struct StackNode<T> {
+    value: T,
+    previous: Option<Box<StackNode<T>>>,
+}
+
+#[derive(Debug)]
+struct Stack<T> {
+    members: Vec<T>,
+    top: Option<T>,
+}
+
+impl<T> Stack<T> {
+    pub fn new() -> Stack<T> {
+        Stack {
+            members: Vec::<T>::new(),
+            top: None,
+        }
+    }
+
+    pub fn push(&mut self, val: T) {
+        self.members.push(val)
+    }
+
+    pub fn pop(&mut self) -> Option<T> {
+        self.members.pop()
+    } 
+
+    pub fn top(&self) -> Option<&T> {
+        self.members.last()
+    }
+}
+
 fn main() {
-        
-    println!("Bye Bye Bevi ;(");
-    
+    let mut books = Stack::<&str>::new();
+    books.push("Chemistry");
+    books.push("Biology");
+    books.push("Literature");
+
+    println!("{:#?}", books);
 }
